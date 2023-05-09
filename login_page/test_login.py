3e8da66904d6f341c2b10d9105ace0dcbfcd0eec
Sylvia Petrova <sylvia.petrova@skuuudle.com>
2023-05-09 03:44:56 +0300

    added issue related solutions

--- login_page/test_login.py
+++ login_page/test_login.py
@@ -57,6 +57,54 @@ class TestLoginPage:
         error_msg = driver.find_element(By.CLASS_NAME, "error-button")
         assert error_msg.is_displayed()
 
+    def test_error_msg_both_blank(self, driver):
+        # Navigate to the login page
+        driver.get("https://www.saucedemo.com")
+
+        # Click the login button
+        login_button = driver.find_element(By.NAME, "login-button")
+        login_button.click()
+
+        # Check that the error message is displayed
+        error_msg = driver.find_element(By.CLASS_NAME, "error")
+        assert error_msg.is_displayed()
+
+    def test_error_msg_password_blank(self, driver):
+        # Navigate to the login page
+        driver.get("https://www.saucedemo.com")
+
+        # Fill in the username field, but leave the password field blank
+        username_field = driver.find_element(By.NAME, "user-name")
+        username_field.send_keys("standard_user")
+
+        # Click the login button
+        login_button = driver.find_element(By.NAME, "login-button")
+        login_button.click()
+
+        # Check that the error message is displayed
+        error_msg_xpath = "//*[contains(text(),'Epic sadface: Password is required')]"
+        error_msg_element = driver.find_element(By.XPATH, error_msg_xpath)
+        error_msg_text = error_msg_element.text
+        assert error_msg_text == "Epic sadface: Password is required"
+
+    def test_error_msg_username_blank(self, driver):
+        # Navigate to the login page
+        driver.get("https://www.saucedemo.com")
+
+        # Fill in the username field, but leave the password field blank
+        username_field = driver.find_element(By.NAME, "password")
+        username_field.send_keys("secret_sauce")
+
+        # Click the login button
+        login_button = driver.find_element(By.NAME, "login-button")
+        login_button.click()
+
+        # Check that the error message is displayed
+        error_msg_xpath = "//*[contains(text(),'Epic sadface: Username is required')]"
+        error_msg_element = driver.find_element(By.XPATH, error_msg_xpath)
+        error_msg_text = error_msg_element.text
+        assert error_msg_text == "Epic sadface: Username is required"
+
     # Test case 4: Login and redirected to home page when credentials are valid
     def test_login_button_enabled_when_complete(self, driver):
         # Navigate to the login page
